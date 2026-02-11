64b30d0e20bd3559c89b0433b7f1055733ad0883
Nguyen-Ba-Minh <1155184240@link.cuhk.edu.hk>
2026-02-11 17:21:01 +0800

    Update on CORS in properties files
    Update on CORS in properties files

--- backend/src/main/java/com/project/matchingengine/controllers/order/PriceController.java
+++ backend/src/main/java/com/project/matchingengine/controllers/order/PriceController.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "${spring.web.cors.allowed-origins}")
 @RequestMapping("api/prices")
 @Profile("!ingress")
 public class PriceController {
